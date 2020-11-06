d39272be2c09a75a54df62c4abb8b3202f517fdf
zero-8 <jmangaluz@gmail.com>
2020-11-06 10:00:16 +0800

    simple upload v 2.0.0

--- src/components/CkeditorNuxt.vue
+++ src/components/CkeditorNuxt.vue
@@ -15,7 +15,10 @@
     // import FullFreeBuildEditor from '@engrjerickcmangalus/ckeditor5-build-custom-simpleuploader'
     // import FullFreeBuildEditor from '@engrjerickcmangalus/ckeditor5-build-custom-inlineuploader'
     // import FullFreeBuildEditor from '@engrjerickcmangalus/ckeditor5-build-custom-ck'
-    import FullFreeBuildEditor from '@engrjerickcmangalus/ckeditor5-build-custom-inline-project'
+    // import FullFreeBuildEditor from '@engrjerickcmangalus/ckeditor5-build-custom-inline-project'
+
+    import FullFreeBuildEditor from '@engrjerickcmangalus/ckeditor5-build-custom-simpleuploader'
+    
     import CKEditor from '@ckeditor/ckeditor5-vue'
 
     export default {
